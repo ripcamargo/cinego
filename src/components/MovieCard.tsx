69d01230e6678a8a17eb89af485ee1c6d5b82db4
Ricardo <ricardopcamargojr@gmail.com>
2026-07-02 23:09:12 -0300

    Implementa tela de listagem com busca e cards de filmes

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,19 +1,22 @@
 import type { Movie } from "../types/Movie"
+import { Link } from 'react-router-dom'
 
 interface Props {
-  movie: Movie
+    movie: Movie
 }
 
 function MovieCard({ movie }: Props) {
-  return (
-    <div className="card">
-  <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} className="card-img-top" alt={movie.title} />
-  <div className="card-body">
-    <h5 className="card-title">{movie.title}</h5>
-    <p className="card-text">{movie.vote_average}</p>
-  </div>
-</div>
-  )
+    return (
+        <Link to={`/movie/${movie.id}`} className="text-decoration-none text-dark">
+            <div className="card">
+                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} className="card-img-top" alt={movie.title} />
+                <div className="card-body">
+                    <h5 className="card-title">{movie.title}</h5>
+                    <p className="card-text">{movie.vote_average}</p>
+                </div>
+            </div>
+        </Link>
+    )
 }
 
 export default MovieCard
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,32 +1,50 @@
 import { useState, useEffect } from 'react'
 import type { Movie } from '../types/Movie'
-import { getPopularMovies } from '../services/tmdb'
+import { getPopularMovies, searchMovies } from '../services/tmdb'
 import MovieCard from '../components/MovieCard'
+
 
 function Home() {
 
     const [movies, setMovies] = useState<Movie[]>([])
+    const [query, setQuery] = useState('')
+
 
     useEffect(() => {
-        async function buscarFilmes() {
+        async function buscar() {
             try {
-                const filmesPopulares = await getPopularMovies()
-                setMovies(filmesPopulares)
+                if (query === '') {
+                    const filmesPopulares = await getPopularMovies()
+                    setMovies(filmesPopulares)
+                } else {
+                    const resultadosBusca = await searchMovies(query)
+                    setMovies(resultadosBusca)
+                }
             } catch (error) {
-                console.error("Erro ao buscar filmes populares:", error)
+                console.error("Erro ao buscar filmes:", error)
             }
         }
 
-        buscarFilmes()
-    }, [])
+        buscar()
+    }, [query])
 
     return (
-        <div className="row">
-            {movies.map((movie) => (
-                <div className="col-md-3" key={movie.id}>
-                    <MovieCard movie={movie} />
-                </div>
-            ))}
+        <div className="container mt-4">
+
+            <input
+                type="text"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                placeholder="Buscar filme..."
+                className="form-control mb-4"
+            />
+            <div className="row">
+                {movies.map((movie) => (
+                    <div className="col-md-3" key={movie.id}>
+                        <MovieCard movie={movie} />
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
